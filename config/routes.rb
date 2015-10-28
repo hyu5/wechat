8ceca70d56c6d3f316f4dcc5ee89c87f938a9a6e
hyu5 <hyu5@students.towson.edu>
2015-10-27 23:41:50 -0400

    copy all Devise views to the application

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   devise_for :admins
-
   get 'home/admin'
 
   devise_for :users
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151025022555) do
+ActiveRecord::Schema.define(version: 20151013202453) do
 
   create_table "admins", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -44,10 +44,6 @@ ActiveRecord::Schema.define(version: 20151025022555) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
-    t.string   "FirstName"
-    t.string   "LastName"
-    t.string   "Phone"
-    t.date     "DateOfBirth"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
