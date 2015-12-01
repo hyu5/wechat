2e0b9b4c1698bb1a9e5bfcd2e0d0a6a0a66567e6
hyu5 <hyu5@students.towson.edu>
2015-12-01 16:43:43 -0500

    remove friendship

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,8 +1,9 @@
 class FriendshipsController < ApplicationController
+
   def create
     @friendship = current_user.friendships.build(:friend_id => params[:friend_id])
     if @friendship.save
-      flash[:notice] = "Added friend."
+      flash[:notice] = "Added friend successfully!"
       redirect_to root_url
     else
       flash[:error] = "Unable to add friend."
@@ -11,9 +12,14 @@ class FriendshipsController < ApplicationController
   end
 
   def destroy
-    @friendship = current_user.friendships.find(params[:id])
-    @friendship.destroy
-    flash[:notice] = "Removed friendship."
-    redirect_to current_user
+    @friendship = current_user.friendships.find_by_friend_id(params[:id])
+    if @friendship.destroy
+      flash[:notice] = "Removed friendship."
+      redirect_to root_url
+    else
+      flash[:error] = "Unable to remove friend."
+      redirect_to root_url
+    end
   end
+
 end
