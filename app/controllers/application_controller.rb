6666e1dd4f71edff593e992b6e5f8d3a97a74b09
hyu5 <hyu5@students.towson.edu>
2015-12-04 20:15:42 -0500

    fixed redirect problems in admin pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,41 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  def after_sign_in_path_for(resource)
+    if resource.class == User
+      root_path
+    elsif resource.class == Admin
+      home_admin_path
+    end
+  end
+
+  def after_sign_up_path_for(resource)
+    if resource.class == User
+      signed_in_root_path
+    elsif resource.class == Admin
+      signed_in_home_admin_path
+    end
+  end
+
+  def after_update_path_for(resource)
+    if resource.class == User
+      signed_in_root_path
+    elsif resource.class == Admin
+      signed_in_home_admin_path
+    end
+  end
+
+  def after_resetting_password_path_for(resource)
+    if resource.class == User
+      signed_in_root_path
+    elsif resource.class == Admin
+      signed_in_home_admin_path
+    end
+  end
+
+  def after_sign_out_path_for(resource)
+    request.referrer
+  end
+
 end
+
