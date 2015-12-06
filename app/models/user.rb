b23bc232762addd90a5a8acbe93080301783a6fe
hyu5 <hyu5@students.towson.edu>
2015-12-05 23:06:01 -0500

    add validation for edit profile

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,4 +12,20 @@ class User < ActiveRecord::Base
   has_and_belongs_to_many :messages
 
   has_many :moments
+
+  validates :email, presence: true
+  validates :email, uniqueness: true
+  validates :FirstName, :LastName, length: {maximum:50}
+  validates :Phone, allow_blank: true, format: {
+                      with:    /[0-9]{3}-[0-9]{3}-[0-9]{4}/,
+                      message: 'Phone number must be in xxx-xxx-xxxx format.'
+                  }
+  validates :DateOfBirth, allow_blank: true, format: {
+                            with:    /\d{4}\-\d{2}\-\d{2}/,
+                            message: 'Date of birth must be in the yyyy-mm-dd format.'
+                        }
+  validates :email, format: {
+                      with:    /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/,
+                      message: 'Please input the valid email address.'
+                  }
 end
